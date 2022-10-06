4378e7ab47e0e75c30c823f00337ee4a40232a69
Jake Slye <61893488+jakeslye@users.noreply.github.com>
2022-10-05 17:45:06 -0700

    added normalize to Vector3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Vector3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Vector3.java
@@ -6,16 +6,24 @@ public class Vector3 extends Vector2{
 
 	public Vector3() {
 		super();
-		this.x = 0.0;
-		this.y = 0.0;
 		this.z = 0.0;
 	}
 
 	public Vector3(double x, double y, double z){
 		super(x, y);
-		this.x = x;
-		this.y = y;
 		this.z = z;
+	}
+
+	@Override
+	public void normalize() {
+		double length = Math.sqrt(x*x + y*y + z*z);
+
+		if (length != 0.0) {
+			double s = 1.0 / (double)length;
+			x = x*s;
+			y = y*s;
+			z = z*s;
+		}
 	}
 
 	@Override
@@ -23,13 +31,13 @@ public class Vector3 extends Vector2{
 		x += other.x;
 		y += other.y;
 		z += other.z;
-    	}
+	}
 	@Override
 	public void multiply(Vector3 other){
 		x *= other.x;
 		y *= other.y;
 		z *= other.z;
-    	}
+	}
 	@Override
 	public void subtract(Vector3 other){
 		x -= other.x;
@@ -37,11 +45,11 @@ public class Vector3 extends Vector2{
 		z -= other.z;
 	}
 	@Override
-    	public void divide(Vector3 other){
+	public void divide(Vector3 other){
 		x /= other.x;
 		y /= other.y;
 		z /= other.z;
-    	}
+	}
 	
 	public String toString() {
 		return "x: " + x + ", y: " + y  + ", z: " + z;
