834207bae6f66db5f268aa2964190b4fa64fe6f1
Jake Slye <61893488+jakeslye@users.noreply.github.com>
2022-10-05 17:33:56 -0700

    quick fix 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Vector3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Vector3.java
@@ -1,4 +1,4 @@
-package utils;
+package org.firstinspires.ftc.teamcode.utils;
 
 public class Vector3 extends Vector2{
 
@@ -10,37 +10,37 @@ public class Vector3 extends Vector2{
 		this.y = 0.0;
 		this.z = 0.0;
 	}
-	
+
 	public Vector3(double x, double y, double z){
 		super(x, y);
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
-	
+
 	@Override
 	public void add(Vector3 other) {
-    	x += other.x;
-    	y += other.y;
-    	z += other.z;
+	x += other.x;
+	y += other.y;
+	z += other.z;
     }
 	@Override
 	public void multiply(Vector3 other){
-        x *= other.x;
-        y *= other.y;
-    	z *= other.z;
+	x *= other.x;
+	y *= other.y;
+	z *= other.z;
     }
 	@Override
 	public void subtract(Vector3 other){
-        x -= other.x;
-        y -= other.y;
-    	z -= other.z;
+	x -= other.x;
+	y -= other.y;
+	z -= other.z;
     }
 	@Override
     public void divide(Vector3 other){
-        x /= other.x;
-        y /= other.y;
-    	z /= other.z;
+	x /= other.x;
+	y /= other.y;
+	z /= other.z;
     }
 	
 	public String toString() {
