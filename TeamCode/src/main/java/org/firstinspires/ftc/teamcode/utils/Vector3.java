ee7262072739d412cb9e6c89f0e39bc107c6766f
Jake Slye <61893488+jakeslye@users.noreply.github.com>
2022-10-05 17:32:23 -0700

    Vector3

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Vector3.java
@@ -0,0 +1,49 @@
+package utils;
+
+public class Vector3 extends Vector2{
+
+	public double z;
+
+	public Vector3() {
+		super();
+		this.x = 0.0;
+		this.y = 0.0;
+		this.z = 0.0;
+	}
+	
+	public Vector3(double x, double y, double z){
+		super(x, y);
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	@Override
+	public void add(Vector3 other) {
+    	x += other.x;
+    	y += other.y;
+    	z += other.z;
+    }
+	@Override
+	public void multiply(Vector3 other){
+        x *= other.x;
+        y *= other.y;
+    	z *= other.z;
+    }
+	@Override
+	public void subtract(Vector3 other){
+        x -= other.x;
+        y -= other.y;
+    	z -= other.z;
+    }
+	@Override
+    public void divide(Vector3 other){
+        x /= other.x;
+        y /= other.y;
+    	z /= other.z;
+    }
+	
+	public String toString() {
+		return "x: " + x + ", y: " + y  + ", z: " + z;
+	}
+}
