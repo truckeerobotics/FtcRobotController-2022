db643929b929dd5459334931a6fe771137e8a185
jakeslye <slyefoxgaming@gmail.com>
2022-10-16 19:35:21 -0700

    Added basic OpMode for scrimmage meet.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Scrimmage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Scrimmage.java
@@ -4,15 +4,36 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 @TeleOp(name = "Scrimmage")
 public class Scrimmage extends LinearOpMode {
     public void runOpMode() throws InterruptedException{
         DcMotor leftMotor = hardwareMap.dcMotor.get("left_drive");
         DcMotor rightMotor = hardwareMap.dcMotor.get("right_drive");
+
+        Boolean tankMode = false;
+
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        telemetry.setAutoClear(false);
+        Telemetry.Item modeItem = telemetry.addData("Mode", "Tank");
+
         waitForStart();
+
         while (!isStopRequested()) {
-            leftMotor.setPower(1);
-            rightMotor.setPower(-1);
+            if(tankMode){
+                modeItem.setValue("Tank");
+                telemetry.update();
+                leftMotor.setPower(gamepad1.left_stick_y);
+                rightMotor.setPower(gamepad2.left_stick_y);
+            }else{
+                //No turning yet
+                modeItem.setValue("Normal");
+                telemetry.update();
+                double power = gamepad1.left_stick_y;
+                leftMotor.setPower(power);
+                rightMotor.setPower(power);
+            }
         }
     }
 }
