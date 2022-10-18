4eb3e84d575ae22a3602a06c072a4a21b291ad53
jakeslye <slyefoxgaming@gmail.com>
2022-10-17 17:58:29 -0700

    Ready for scrimmage pt 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Scrimmage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Scrimmage.java
@@ -14,10 +14,6 @@ public class Scrimmage extends LinearOpMode {
         DcMotor leftMotor = hardwareMap.dcMotor.get("left_drive");
         DcMotor rightMotor = hardwareMap.dcMotor.get("right_drive");
 
-        Boolean tankMode = false;
-
-        DriverInput inputHelper = new DriverInput(gamepad1, gamepad2);
-
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
 
         telemetry.setAutoClear(false);
@@ -26,35 +22,16 @@ public class Scrimmage extends LinearOpMode {
 
         waitForStart();
 
-        Telemetry.Item modeItem = telemetry.addData("Mode", "Normal");
         debugItem.setValue("Main Loop is Running!");
         telemetry.update();
 
         while (!isStopRequested()) {
-            inputHelper.update();
-
-            if(tankMode){
-                modeItem.setValue("Tank");
-                telemetry.update();
-                leftMotor.setPower(gamepad1.left_stick_y);
-                rightMotor.setPower(gamepad1.right_stick_y);
-            }else{
-                modeItem.setValue("Normal");
-                telemetry.update();
-                double drive = -gamepad1.left_stick_y;
-                double turn = gamepad1.right_stick_x;
-                double leftPower = Range.clip(drive + turn, -1.0, 1.0);
-                double rightPower = Range.clip(drive - turn, -1.0, 1.0);
-                leftMotor.setPower(leftPower);
-                rightMotor.setPower(rightPower);
-            }
-            if(inputHelper.onPush("controller1ButtonX")){
-                if(tankMode) {
-                    tankMode = false;
-                }else{
-                    tankMode = true;
-                }
-            }
+            double drive = -gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+            double leftPower = Range.clip(drive + turn, -1.0, 1.0);
+            double rightPower = Range.clip(drive - turn, -1.0, 1.0);
+            leftMotor.setPower(leftPower);
+            rightMotor.setPower(rightPower);
         }
     }
 }
