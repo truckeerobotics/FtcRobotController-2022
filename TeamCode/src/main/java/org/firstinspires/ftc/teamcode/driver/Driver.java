cd2c0c814b2152c12f16416f109a77e01df609e1
tysuh66 <suhtyson@gmail.com>
2022-10-31 15:46:05 -0700

    jake stop deleting my comments you bum

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driver/Driver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driver/Driver.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.driver;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -32,6 +33,21 @@ public class Driver {
     }
 
     public void run(){
+
+        /*
+        UNCOMMENT WHEN MECHANUM SWITCH IS MADE
+
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+
+        motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotor.Direction.REVERSE);
+
+        also delete the below
+        */
+
         DcMotor leftMotor = opmode.hardwareMap.dcMotor.get("left_drive");
         DcMotor rightMotor = opmode.hardwareMap.dcMotor.get("right_drive");
 
@@ -53,10 +69,12 @@ public class Driver {
             double x = xCurrent;
             double rx = rxCurrent;
 
+            // Smart/Smooth driving logic
 
-            yTarget = -opmode.gamepad1.left_stick_y * LateralSpeed;
+            yTarget = -opmode.gamepad1.left_stick_y * LateralSpeed; // Remember, this is reversed!
             rxTarget = -opmode.gamepad1.right_stick_x * RotationalSpeed;
 
+            // If difference cutoff is not reached, then move current/actual movement to target movement.
             if (!(Math.abs(yTarget - yCurrent) < drivingPowerDifferenceCutoff)) {
                 if (yTarget > yCurrent) {
                     yCurrent += drivingPowerForwardDelta;
@@ -84,6 +102,68 @@ public class Driver {
             double rightPower = Range.clip(drive - turn, -1.0, 1.0);
             leftMotor.setPower(leftPower/denominator);
             rightMotor.setPower(rightPower/denominator);
+
+            //FUTURE DRIVE CODE
+
+            //--------------------------------------------------------
+
+            /*
+            yTarget = -opmode.gamepad1.left_stick_y * LateralSpeed; // Remember, this is reversed!
+            xTarget = -opmode.gamepad1.left_stick_x * StrafeSpeed * 1.1; // Counteract imperfect strafing
+            rxTarget = -opmode.gamepad1.right_stick_x * RotationalSpeed;
+
+            // If difference cutoff is not reached, then move current/actual movement to target movement.
+            if (!(Math.abs(yTarget - yCurrent) < drivingPowerDifferenceCutoff)) {
+                if (yTarget > yCurrent) {
+                    yCurrent += drivingPowerForwardDelta;
+                } else {
+                    yCurrent -= drivingPowerBackwardDelta;
+                }
+            } else {
+                yCurrent = yTarget;
+            }
+
+            if (!(Math.abs(xTarget - xCurrent) < drivingPowerDifferenceCutoff)) {
+                if (xTarget > xCurrent) {
+                    xCurrent += drivingPowerStrafeDelta;
+                } else {
+                    xCurrent -= drivingPowerStrafeDelta;
+                }
+            } else {
+                xCurrent = xTarget;
+            }
+
+            if (!(Math.abs(rxTarget - rxCurrent) < drivingPowerDifferenceCutoff)) {
+                if (rxTarget > rxCurrent) {
+                    rxCurrent += drivingPowerRotationDelta;
+                } else {
+                    rxCurrent -= drivingPowerRotationDelta;
+                }
+            } else {
+                rxCurrent = rxTarget;
+            }
+            // Take things from smart/smooth driving and implement the intended axial changes.
+
+            double y = yCurrent;
+            double x = xCurrent;
+            double rx = rxCurrent;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio, but only when
+            // at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            //uncomment later --------------------------------------------------------------------------------
+
+//            motorFrontLeft.setPower(frontLeftPower);
+//            motorBackLeft.setPower(backLeftPower);
+//            motorFrontRight.setPower(frontRightPower);
+//            motorBackRight.setPower(backRightPower);
+             */
         }
     }
 }
