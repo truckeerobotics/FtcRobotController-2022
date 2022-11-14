77a5bf1fb8757ba6cc0be0138ad3faaf3a62ea14
truckeerobotics-team <truckeerobotics@gmail.com>
2022-11-13 16:29:25 -0800

    arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driver/Driver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driver/Driver.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.driver;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
@@ -33,81 +34,39 @@ public class Driver {
     }
 
     public void run(){
-
-        /*
-        UNCOMMENT WHEN MECHANUM SWITCH IS MADE
-
-        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
-        DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
-        DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
-        DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+        DcMotor motorFrontLeft = opmode.hardwareMap.dcMotor.get("motorFrontLeft");
+        DcMotor motorBackLeft = opmode.hardwareMap.dcMotor.get("motorBackLeft");
+        DcMotor motorFrontRight = opmode.hardwareMap.dcMotor.get("motorFrontRight");
+        DcMotor motorBackRight = opmode.hardwareMap.dcMotor.get("motorBackRight");
+        DcMotor armLeft = opmode.hardwareMap.dcMotor.get("armLeft");
+        DcMotor armRight = opmode.hardwareMap.dcMotor.get("armRight");
 
         motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
         motorBackRight.setDirection(DcMotor.Direction.REVERSE);
-
-        also delete the below
-        */
-
-        DcMotor leftMotor = opmode.hardwareMap.dcMotor.get("left_drive");
-        DcMotor rightMotor = opmode.hardwareMap.dcMotor.get("right_drive");
-
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        armLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
         opmode.waitForStart();
 
         DriverInput input = new DriverInput(opmode.gamepad1, opmode.gamepad2);
         Boolean toggle = false;
         while (!opmode.isStopRequested()) {
-            opmode.telemetry.addData("toggle", toggle);
+/*            opmode.telemetry.addData("toggle", toggle);
             opmode.telemetry.addData("gamepad1x", opmode.gamepad1.x);
             opmode.telemetry.update();
-            if(input.onPush(opmode.gamepad1.x, "controller1ButtonX")){
+           if(input.onPush(opmode.gamepad1.x, "controller1ButtonX")){
                 toggle = !toggle;
-            }
+            }*/
+
+            armLeft.setPower(opmode.gamepad2.left_stick_y);
+            armRight.setPower(opmode.gamepad2.left_stick_y);
 
             double y = yCurrent;
             double x = xCurrent;
             double rx = rxCurrent;
 
-            // Smart/Smooth driving logic
 
-            yTarget = -opmode.gamepad1.left_stick_y * LateralSpeed; // Remember, this is reversed!
-            rxTarget = -opmode.gamepad1.right_stick_x * RotationalSpeed;
 
-            // If difference cutoff is not reached, then move current/actual movement to target movement.
-            if (!(Math.abs(yTarget - yCurrent) < drivingPowerDifferenceCutoff)) {
-                if (yTarget > yCurrent) {
-                    yCurrent += drivingPowerForwardDelta;
-                } else {
-                    yCurrent -= drivingPowerBackwardDelta;
-                }
-            } else {
-                yCurrent = yTarget;
-            }
 
-            if (!(Math.abs(rxTarget - rxCurrent) < drivingPowerDifferenceCutoff)) {
-                if (rxTarget > rxCurrent) {
-                    rxCurrent += drivingPowerRotationDelta;
-                } else {
-                    rxCurrent -= drivingPowerRotationDelta;
-                }
-            } else {
-                rxCurrent = rxTarget;
-            }
-
-            double drive = yCurrent;
-            double turn = -rxCurrent;
-            double denominator = Math.max(Math.abs(y) + Math.abs(rx), 1);
-            double leftPower = Range.clip(drive + turn, -1.0, 1.0);
-            double rightPower = Range.clip(drive - turn, -1.0, 1.0);
-            leftMotor.setPower(leftPower/denominator);
-            rightMotor.setPower(rightPower/denominator);
-
-            //FUTURE DRIVE CODE
-
-            //--------------------------------------------------------
-
-            /*
             yTarget = -opmode.gamepad1.left_stick_y * LateralSpeed; // Remember, this is reversed!
             xTarget = -opmode.gamepad1.left_stick_x * StrafeSpeed * 1.1; // Counteract imperfect strafing
             rxTarget = -opmode.gamepad1.right_stick_x * RotationalSpeed;
@@ -144,10 +103,6 @@ public class Driver {
             }
             // Take things from smart/smooth driving and implement the intended axial changes.
 
-            double y = yCurrent;
-            double x = xCurrent;
-            double rx = rxCurrent;
-
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
@@ -157,13 +112,10 @@ public class Driver {
             double frontRightPower = (y - x - rx) / denominator;
             double backRightPower = (y + x - rx) / denominator;
 
-            //uncomment later --------------------------------------------------------------------------------
-
-//            motorFrontLeft.setPower(frontLeftPower);
-//            motorBackLeft.setPower(backLeftPower);
-//            motorFrontRight.setPower(frontRightPower);
-//            motorBackRight.setPower(backRightPower);
-             */
+            motorFrontLeft.setPower(frontLeftPower);
+            motorBackLeft.setPower(backLeftPower);
+            motorFrontRight.setPower(frontRightPower);
+            motorBackRight.setPower(backRightPower);
         }
     }
 }
